b6892ac4e12ed4b0bf9b7e8ed05e8f6a9761d1a9
Seleini2 <ghibli@telia.com>
2024-12-16 16:30:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import React, { useEffect } from 'react';
 
-const client = generateClient<Schema>();
+const RedirectComponent: React.FC = () => {
+    useEffect(() => {
+        // Get the last part of the current URL path
+        const path = window.location.pathname.split('/').pop();
 
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+        if (path) {
+            // Redirect to Google with the dynamic path value
+            window.location.href = `https://yiport.com/${path}`;
+        }
+    }, []);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    return null; // No UI is needed
+};
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        Hey
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
-}
-
-export default App;
+export default RedirectComponent;
