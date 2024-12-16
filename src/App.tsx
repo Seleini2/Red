9875db2437a89a4bbc736742f02d45efe92b4d6a
Seleini2 <ghibli@telia.com>
2024-12-16 16:51:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
-const Home: React.FC = () => {
-    return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the main page of your application.</p>
-        </div>
-    );
-};
+const client = generateClient<Schema>();
 
-const RedirectComponent: React.FC = () => {
-    React.useEffect(() => {
-        const path = window.location.pathname.split('/').pop();
-        if (path) {
-            window.location.href = `https://google.com/${path}`;
-        }
-    }, []);
-    return null;
-};
+function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-const App: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                {/* Route for the home page */}
-                <Route path="/" element={<Home />} />
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
 
-                {/* Route for dynamic redirects */}
-                <Route path="/:dynamicValue" element={<RedirectComponent />} />
-            </Routes>
-        </Router>
-    );
-};
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+
+  return (
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        Hey
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+    </main>
+  );
+}
 
 export default App;
